a4f0346e7e26afe4ac8b703a7f4c026d07e00090
AhmedY29 <160507890+AhmedY29@users.noreply.github.com>
2024-11-01 16:24:48 +0300

    Update authStore.js

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -16,7 +16,7 @@ export const useAuthStore = create((set)=>({
         set({isLoading:true, error:null})
         try {
             const response = await axios.post(`${API_URL}/register`, {email, password, name})
-            set({user:response.data.user, isAuthenticated: true , isLoading:false})
+            set({user:response.data.user , isLoading:false})
         } catch (error) {
             set({error:error.response.data.message || "Error in  Registering ", isLoading:false})
             throw error
